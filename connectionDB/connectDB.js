92efdb52fa97b88069ca54b2f3c87e94dfcaea17
Javaria Rizwan <javariarizwan332@gmail.com>
2025-10-05 13:46:24 +0500

    Update connectDB.js

--- connectionDB/connectDB.js
+++ connectionDB/connectDB.js
@@ -1,7 +1,9 @@
 const mongoose=require("mongoose");
+require("dotenv").config();
+
 const connect = async()=>{
     try {
-    const con=await mongoose.connect(`mongodb://localhost:27017/nexus_ai`);
+    const con=await mongoose.connect(process.env.MONGODB_URI);
     console.log("Connection was successful.");    
     } catch (error) {
         console.log("❌ Error connecting to MongoDB:", error.message)
